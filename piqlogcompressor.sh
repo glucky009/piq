c6265b10c5251fddada2eb6aec4df64c102d8143
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-12 12:14:07 +1100

    Update piqlogcompressor.sh

--- piqlogcompressor.sh
+++ piqlogcompressor.sh
@@ -5,7 +5,7 @@
 ## Version : 1.00
 ## Description : Compresses the logs files and removes them
 ## 
-## Add this line in "crontab -e" to run every month "5 * * * * /bin/bash /opt/rpstrata/piqlogcompressor.sh"
+## Add this line in "crontab -e -u root" to run every month "7 1 1 * * /bin/bash /opt/rpstrata/piqlogcompressor.sh"
 ##
 ## Note: This scipt will be ran every month to compress the log files
 ##
