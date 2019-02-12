e707e35c2eb251383e95fdc6b53802ba6c1678bd
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-12 11:19:42 +1100

    Create piqlogcompressor.sh

---
+++ piqlogcompressor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+############################################################################
+## Author :PIQ Support
+## Date : 11/02/2019 	
+## Version : 1.00
+## Description : Compresses the logs files and removes them
+## 
+## Add this line in "crontab -e" to run every month "5 * * * * /bin/bash /opt/rpstrata/piqlogcompressor.sh"
+##
+## Note: This scipt will be ran every month to compress the log files
+##
+###########################################################################
+
+#Variables
+
+piqlogFiles=/var/log/rpstrata/piqlogs[0-2]*.csv
+dateMonth=$(date +"%Y-%m")
+
+#Compressor
+tar cfzvP /var/log/rpstrata/piqlogs$dateMonth.tar.gz $piqlogFiles --remove-files
