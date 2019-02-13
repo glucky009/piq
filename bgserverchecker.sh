3f1e9fa6a84ff1106ec23c1f0b723917065dbb10
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-13 12:43:45 +1100

    Update bgserverchecker.sh

--- bgserverchecker.sh
+++ bgserverchecker.sh
@@ -6,7 +6,6 @@ until [ $n -ge 5 ]
 do
     bgServer=$(ps aux | grep BG | wc -l)
     if [[ $bgServer -eq 1 ]]; then
-      
         rm -rf /opt/rpstrata/root/bg.pid
         rm -rf /opt/rpstrata/root/bg_nohup.log
         /bin/bash /opt/rpstrata/bgserver_start.sh
