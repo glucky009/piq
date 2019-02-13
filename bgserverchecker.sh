73be0e358d9d131521cd65054c379fc4ec9df757
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-13 13:13:33 +1100

    Update bgserverchecker.sh

--- bgserverchecker.sh
+++ bgserverchecker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+############################################################################
+## Author :PIQ Support
+## Date : 13/02/2019	
+## Version : 1.0
+## Description : Checks if the bgServer service is running and attempts to start it if not and will retry 5 times if not
+## 
+## Add this line in "crontab -e" of root to run every hour "3 * * * * /bin/bash /opt/rpstrata/piqstatus/bgserverchecker.sh"
+##
+###########################################################################
+
 n=0
 
 until [ $n -ge 5 ]
