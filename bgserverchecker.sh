dcb3f82bd4ace7b1adb1e25e86ed723d9a84923b
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-13 12:40:57 +1100

    Update bgserverchecker.sh

--- bgserverchecker.sh
+++ bgserverchecker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-bgServer=$(ps aux | grep BG | wc -l)
+n=0
 
-while [[ $bgServer -eq 1 ]]; 
-    do
-        if [[ $bgServer -eq 1 ]]; then
-            rm -rf /opt/rpstrata/root/bg.pid
-            rm -rf /opt/rpstrata/root/bg_nohup.log
-            /bin/bash /opt/rpstrata/bgserver_start.sh
-        break
-        elif [[ $bgServer -eq 2 ]]; then
-            echo "BG Server is already running"
-        else
-            echo "BG Server is already running"
-        fi
+until [ $n -ge 5 ]
+do
+    bgServer=$(ps aux | grep BG | wc -l)
+    if [[ $bgServer -eq 1 ]]; then
+      
+        rm -rf /opt/rpstrata/root/bg.pid
+        rm -rf /opt/rpstrata/root/bg_nohup.log
+        /bin/bash /opt/rpstrata/bgserver_start.sh
+        n=$[n+1]
+        sleep 5
+    else
+        n=$[n+5]
+        echo "BGServer is now running"
+    fi
 done
