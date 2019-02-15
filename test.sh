72dc8ed1ce0fd8883ab9cfa12230bd01016c0fb8
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-15 12:29:15 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,63 @@
-#!/bin/bash
-if 
+#!/bin/sh
+############################################################################
+## Author :PIQ Support
+## Date : 11/02/2019 	
+## Version : 1.00
+## Description : Compresses the logs files and removes them
+## 
+## Add this line in "crontab -e -u root" to run every month "7 1 1 * * /bin/bash /opt/rpstrata/piqlogcompressor.sh"
+##
+## Note: This scipt will be ran every month to compress the log files
+##
+###########################################################################
+
+#Variables
+
+piqlogFiles=/var/log/rpstrata/piqlogs[0-2]*.csv
+dateMonth=$(date +"%Y-%m")
+dateMonthTime=$(date +"%D %T")
+tarLogLocation="/var/log/rpstrata/tarLog.csv"
+tarErrorLocation="/var/log/rpstrata/tarErrorLog.csv"
+
+#Function
+Compress ()
+{
+    cfzvP /var/log/rpstrata/piqlogs$dateMonth.tar.gz $piqlogFiles  
+}
+
+Output ()
+{
+if [ -f $tarLogLocation ]; then
+    Compress > /dev/null 
+    for p in $(ls $piqlogFiles)
+    do
+        echo "$dateMonthTime,$p" >> $tarLogLocation
+    done
+else
+    Compress > /dev/null
+    echo "Date, CSV File" > $tarLogLocation
+    for p in $(ls $piqlogFiles)
+    do
+        echo "$dateMonthTime,$p" >> $tarLogLocation
+    done
+fi
+
+}
+
+#Phase 2
+{ 
+
+Output
+
+} && {
+rm -rf $piqlogFiles
+} t
+} || { 
+
+if [ -f $tarErrorLocation ]; then
+    echo $dateMonthTime >> $tarErrorLocation
+    Compress 2>> $tarErrorLocation
+else
+    echo $dateMonthTime > $tarErrorLocation
+    Compress 2>> $tarErrorLocation
+fi }
