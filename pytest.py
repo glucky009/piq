d12959f0ac7fb59c434dbf26aa6babb6681aa033
Karlo Franco Victorio <karlo@digitalreign.io>
2019-02-27 12:03:34 +1100

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -6,12 +6,25 @@ import sys
 import datetime
 import subprocess
 import dmidecode
+import fnmatch
 
 #Function
 # This function only applies to Windows and Linux
 def linuxCommand (commandList):
     proc = subprocess.Popen(commandList, stdout=subprocess.PIPE)
     return proc.stdout.read().strip('\n')
+
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(name)
+    return result
+
+def file_size(fname):
+    statinfo = os.stat(fname)
+    return statinfo.st_size
 
 # Variables
 piqVersionList = linuxCommand(["ls", "-l", "/opt/rpstrata/root"])
@@ -52,21 +65,25 @@ dbDiskUsedGB = round(float(dbDiskUsed)/1000000, 2)
 dbDiskTotal = diskInfo[319:328]
 dbDiskTotalGB = round(float(dbDiskTotal)/1000000, 2)
 
+backupFind = find("rpstrata_[0-2][0-9][0-9][0-9]*.sql",'/')
+backupLatest = (backupFind[0])
+backupSize = linuxCommand(["find" ,"/" , "-name", backupLatest])
+backupTotalGB = round(float(file_size(backupSize))/1000000000, 2)
 
 ##
 
 if os.path.exists(piqLogfile):
     print("Adding new log to "+piqLogfile )
     myfile = open(piqLogfile, 'a+')
-    myfile.write("""%s,%s,%s %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
-""" % (dateNow,timeNow,piqVersion,piqVersionDate,cpuModel2,cpuNumber,cpuGHz,cpuIdle,memUsedGHz,memTotalGHz,bootDiskUsedGB,bootDiskTotalGB,diskUsedGB,diskTotalGB,dbDiskUsedGB,dbDiskTotalGB))
+    myfile.write("""%s,%s,%s %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
+""" % (dateNow,timeNow,piqVersion,piqVersionDate,cpuModel2,cpuNumber,cpuGHz,cpuIdle,memUsedGHz,memTotalGHz,bootDiskUsedGB,bootDiskTotalGB,diskUsedGB,diskTotalGB,dbDiskUsedGB,dbDiskTotalGB,backupTotalGB))
     myfile.close
 else:
     print("Creating log file "+piqLogfile)
     myfile = open(piqLogfile, 'w+')
-    myfile.write("Dates, Time, PIQ Version, CPU Model, CPU(s), CPU(GHz), CPU Idle Time (%), RAM Used (GB), RAM Total (GB), Boot Disk Used (GB), Boot Disk Total (GB), Disk Used (GB), Disk Total (GB), DB Disk Used (GB), DB Disk Total (GB), BUP Size (GB)")
+    myfile.write("Date,Time,PIQ Version,CPU Model,CPU(s),CPU (GHz), CPU Idle Time (%), RAM Used (GB), RAM Total (GB), Boot Disk Used (GB), Boot Disk Total (GB),Disk Used (GB), Disk Total (GB),DB Disk Used (GB),DB Disk Total (GB),BUP Size (GB)")
     myfile = open(piqLogfile, 'a+')
     myfile.write("""
-%s,%s,%s %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
-""" % (dateNow,timeNow,piqVersion,piqVersionDate,cpuModel2,cpuNumber,cpuGHz,cpuIdle,memUsedGHz,memTotalGHz,bootDiskUsedGB,bootDiskTotalGB,diskUsedGB,diskTotalGB,dbDiskUsedGB,dbDiskTotalGB))
+%s,%s,%s %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
+""" % (dateNow,timeNow,piqVersion,piqVersionDate,cpuModel2,cpuNumber,cpuGHz,cpuIdle,memUsedGHz,memTotalGHz,bootDiskUsedGB,bootDiskTotalGB,diskUsedGB,diskTotalGB,dbDiskUsedGB,dbDiskTotalGB,backupTotalGB))
     myfile.close
